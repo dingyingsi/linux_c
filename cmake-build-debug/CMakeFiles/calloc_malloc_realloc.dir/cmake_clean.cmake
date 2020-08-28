3b86c16c9c238ed7a7240f0f8bb6dadd12285700
dingyingsi <dingyingsi@126.com>
2020-08-28 10:33:35 +0800

    doc

---
+++ cmake-build-debug/CMakeFiles/calloc_malloc_realloc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calloc_malloc_realloc.dir/c/calloc_malloc_realloc/calloc_malloc_realloc.c.o"
+  "calloc_malloc_realloc"
+  "calloc_malloc_realloc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calloc_malloc_realloc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
